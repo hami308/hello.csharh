8cf736a955f7877354da0e09a4d0b0eb3e2a91f6
LENOVO <LENOVO@LAPTOP-JKMABAVK>
2023-09-19 20:23:18 +0700

    Add project files.

---
+++ hello.csharh/chuongtrinhdau.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+partial class chuongtrinhdau
+{
+    static void Main()
+    {
+        // In ra một dòng văn bản
+        Console.WriteLine("Chao mung ban den voi chuong trinh C#!");
+
+    }
+}
